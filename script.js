8a5d3f918d892fdb5bff4970dfa933a7896d21bd
ramapoltek <158381338+ramapoltek@users.noreply.github.com>
2024-05-11 22:41:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-
-
-        function payWithEWallet() {
-            var vehicleType = document.getElementById('vehicleType').value;
-            var paymentStatus = document.getElementById('paymentStatus');
-            
-            if (vehicleType === "0") {
-                paymentStatus.innerHTML = "Silahkan pilih jenis kendaraan terlebih dahulu.";
-            } else {
-                // Simulasi proses pembayaran
-                paymentStatus.innerHTML = "Pembayaran Rp" + vehicleType + " berhasil menggunakan E-Wallet.";
-                // Anda bisa menambahkan integrasi API pembayaran e-wallet di sini
-            }
-        }
-    </script>
-</body>
-</html>
-
+function payWithEWallet() {
+    var vehicleType = document.getElementById('vehicleType').value;
+    var paymentStatus = document.getElementById('paymentStatus');
+    
+    if (vehicleType === "0") {
+        paymentStatus.innerHTML = "Silahkan pilih jenis kendaraan terlebih dahulu.";
+    } else {
+        // Simulasi proses pembayaran
+        paymentStatus.innerHTML = "Pembayaran Rp" + vehicleType + " berhasil. Silakan lanjutkan pembayaran.";
+        alert("Terima Kasih"); // Menambahkan alert "Terima Kasih"
+        
+        // Redirect ke payment link Midtrans
+        window.location.href = "https://app.sandbox.midtrans.com/payment-links/1715437954712";
+    }
+}
